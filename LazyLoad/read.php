f1f2e2e0893914739715943b9425dcd68999adc5
denizsahingoz <66387056+denizsahingoz@users.noreply.github.com>
2020-08-22 18:43:18 +0300

    update

---
+++ LazyLoad/read.php
@@ -0,0 +1,16 @@
+<?php 
+
+$fileList = glob('*');
+ 
+//Loop through the array that glob returned.
+foreach($fileList as $filename){
+   //Simply print them out onto the screen.
+    $posts[] = array('lazyload'=> $filename, 'url'=> $filename.'_high','tag'=>$filename,'key'=>$filename);
+}
+
+$response['posts'] = $posts;
+
+$fp = fopen('results.json', 'w');
+fwrite($fp, json_encode($response));
+fclose($fp);
+
